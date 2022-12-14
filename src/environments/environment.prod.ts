10db4785a661dfee7df4bfa787de395d29782dc3
Bernardo Afonso <bernardoafonso9734@gmail.com>
2022-12-14 20:25:33 -0300

    correcoes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDhNbKEdycRi_qOlghBbjZDJ5CRQYfVaPQ",
+    authDomain: "gameclips-81cbe.firebaseapp.com",
+    projectId: "gameclips-81cbe",
+    storageBucket: "gameclips-81cbe.appspot.com",
+    appId: "1:992901782284:web:3ada4013a05fcf40694729"
+  }
 };
